d0991b6ac00e71c88f48eb20f7b82ecce3c6cb64
Timothy C. Arland <tcarland@gmail.com>
2024-09-09 13:43:34 -0600

    add config tool readme updates

--- README.md
+++ README.md
@@ -1,11 +1,65 @@
-Wireguard Wrapper
+Wireguard Manager
 =================
 
 A tool wrapping the Wireguard CLI for creating and managing 
 Wireguard tunnels.
 
-Why the wrapper? Wireguard tunnels are easy to create and the 
-CLI easily scriptable. This wrapper provides a consistent 
-configuration with some reasonable defaults that allow for 
-further defining a Wireguard mesh network more consistently.
+Why the wrapper?   
 
+Wireguard tunnels are easy to create and the CLI easily scriptable. 
+This wrapper defines a declaritive configuration (yaml) to represent 
+wireguard tunnels. This allows for defining more complex meshes
+and automation of wireguard tunnels in a clean and consistent manner.
+
+
+# Configuration
+
+The script uses a YAML Configuration to define a host configuration 
+with the following schema:
+```yaml
+---
+wireguard:
+  wg0:
+    addr: "10.200.8.1/24"
+    port: "55820"
+    privatekeyfile: "/root/.wg_pvt.key"
+    publickeyfile: "/root/.wg_pub.key"
+
+    peers:
+      host1:
+        addr: "10.200.8.2"
+        pubkey: "somepubkey"
+        endpoint: "remoteip:port"
+        keepalive: 0
+        default: false
+        allowed_ips:
+          - "10.200.8.2/32"
+          - "172.18.0.0/24"
+        routes:
+          - "172.18.0.0/24"
+```
+
+An initial configuration can be created using the *wg-config.sh* script.
+The script also supports adding a peer to an existing config.
+```sh
+$ wg-config.sh -a 10.0.0.1/24 -p 55820 -i wg0 -c hostcfg.yaml create
+wg-config.sh created config 'hostcfg2.yaml'
+
+$ wg-config.sh -a 10.0.0.2 -i wg0 -c hostcfg.yaml addPeer mypeer mypeerpublickey
+wg-config.sh added peer config for 'mypeer'
+
+$ cat hostcfg.yaml
+---
+wireguard:
+  wg0:
+    addr: 10.0.0.1/24
+    port: 55820
+    privatekeyfile: "/home/tca/.wg_pvt.key"
+    publickeyfile: "/home/tca/.wg_pub.key"
+    peers:
+      mypeer:
+        addr: 10.0.0.2
+        pubkey: mypeerpublickey
+        allowed_ips:
+          - 10.0.0.2/32
+```
--- wg-config.sh
+++ wg-config.sh
@@ -1,25 +1,163 @@
 #!/usr/bin/env bash
 #
 # wg-config.sh
+PNAME=${0##*\/}
+AUTHOR="Timothy C. Arland  <tcarland@gmail.com>"
+VERSION="v24.09.09"
 
+addr=
+iface=
+port=55820
+pvtkeyfile="${HOME}/.wg_pvt.key"
+pubkeyfile="${HOME}/.wg_pub.key"
+endpoint=
+peerkey=
+keepalive=0
 
 
 usage="
-Creates or updates a configuration for use with the Wireguard wrapper.
+Create or Updates a configuration for use with the Wireguard Manager.
 
 Synopsis:
 wg-config.sh [options] <action>
 
 Options:
-  -E|--endpoint
-  -i|--interface
-  -p|--peer
-  -P|--peerkey
+  -a|--addr   <addr>        : The iface or peer IP Address
+  -c|--config <file>        : Path to the yaml config to create or add
+  -E|--endpoint <host:port> : Set a peer endpoint when using 'addPeer'
+  -i|--interface <iface>    : Sets the interface to create or add to.
+  -k|--keepalive <val>      : Set the peer keepalive value (default: 0)
 
 Actions:
-  create   <if> <addr> <port>
-  createFrom <config> <name>
-  addPeer    <name> <addr> <key>
+  create                    : Creates a new yaml config
+  addPeer <name> <key>      : Adds a peer object to a config
 "
 
 
+
+# ----------------------------------------
+# MAIN
+rt=0
+
+if [ -f /etc/${configname} ]; then
+    config="/etc/${config}"
+    echo "$PNAME using system config '$config'"
+fi
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -a|--addr)
+        addr="$2"
+        shift
+        ;;
+    -c|--config)
+        config="$2"
+        shift
+        ;;
+    -E|--endpoint)
+        endpoint="$2"
+        shift
+        ;;
+    'help'|-h|--help)
+        echo "$usage"
+        exit 0
+        ;;
+    -i|--int*)
+        iface="$2"
+        shift
+        ;;
+    -k|--keepalive)
+        keepalive=$2
+        shift
+        ;;
+    -p|--port)
+        port=$2
+        shift
+        ;;
+    'version'|-V|--version)
+        printf "$PNAME $VERSION \n"
+        exit 0
+        ;;
+    *)
+        action="$1"
+        name="$2"
+        peerkey="$3"
+        shift $#
+        ;;
+    esac
+    shift
+done
+
+
+if [ -z "$action" ]; then
+    echo "$PNAME Error, action not provided"
+    exit 1
+fi
+
+if [ -z "$config" ]; then
+    echo "$PNAME Error, config file not provided"
+    exit 1
+fi
+
+case "$action" in
+
+'create')
+    if [ -e $config ]; then
+        echo "$PNAME Error, config file already exists: '$config'"
+        exit 1
+    fi
+    if [[ -z "$addr" ]]; then
+        echo "$PNAME Error, 'create' missing address"
+        exit 1
+    fi
+
+    if [[ "$iface" =~ '^wg\d+' ]]; then
+        echo "$PNAME Error, interface must follow wgX naming convention"
+        exit 2
+    fi
+
+    cat >$config <<EOF
+---
+wireguard:
+  $iface:
+    addr: $addr
+    port: $port
+    privatekeyfile: "$pvtkeyfile"
+    publickeyfile: "$pubkeyfile"
+EOF
+
+    echo "$PNAME created config '$config'"
+    ;;
+
+addPeer)
+    if [ -z "$iface" ]; then
+        echo "$PNAME Error, interface must be provided to addPeer"
+        exit 2
+    fi
+    if [[ -z "$addr" || -z "$peerkey" ]]; then
+        echo "$PNAME Error, 'addPeer' missing arguments"
+        exit 2
+    fi
+    echo "foo"
+    ( yq eval ".wireguard.${iface}.peers.$name = { \"addr\": \"$addr\", \"pubkey\": \"$peerkey\" }" -i $config )
+
+    if [ -n "$endpoint" ]; then
+        ( yq eval ".wireguard.${iface}.peers.$name.endpoint = $endpoint" -i $config )
+    fi
+    if [ $keepalive -gt 0 ]; then
+        ( yq eval ".wireguard.${iface}.peers.$name.keepalive = $keepalive" -i $config )
+    fi
+    
+    ( yq eval ".wireguard.${iface}.peers.$name.default = false" -i $config )
+    ( yq eval ".wireguard.${iface}.peers.$name.allowed_ips = [ \"${addr}/32\" ]" -i $config )
+
+    echo "$PNAME added peer config for '$name'"
+    ;;
+
+*)
+    echo "$PNAME Error, action not recognized"
+    rt=1
+    ;;
+esac
+
+exit $rt
